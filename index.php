0f48bdcfc0d90fba71073473d80d027f56d70028
AlessP93 <alespiscino@libero.it>
2022-06-22 17:16:23 +0200

    fine snack 1 e 2

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+    $teamNba = [
+        [
+            "homeTeam" => "Los Angeles Lakers",
+            "homePoint" => "109",
+            "awayTeam" => "Boston Celtics",
+            "awayPoint" => "98"
+        ],
+        [
+            "homeTeam" => "Miami Heats",
+            "homePoint" => "120",
+            "awayTeam" => "Chiagaco Bulls",
+            "awayPoint" => "101"
+        ],
+        [
+            "homeTeam" => "Toronto Raptors",
+            "homePoint" => "89",
+            "awayTeam" => "Brooklyn Nets",
+            "awayPoint" => "92"
+        ],
+    ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nba Calendar</title>
+</head>
+<body>
+    <div>
+    <h1>NBA: Le partite di oggi sono:</h1>
+        <?php for ($i=0; $i < count($teamNba); $i++) { 
+            echo $teamNba[$i]["homeTeam"] . " " . "|"  . " " . $teamNba[$i]["homePoint"];
+            echo "<br>";
+            echo $teamNba[$i]["awayTeam"] . " " . "|"  . " " . $teamNba[$i]["awayPoint"];
+            echo "<br>";
+            echo "<br>";
+        } ?>
+    </div>
+</body>
+</html>
